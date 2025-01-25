7fad0081c375c16687a816d18398c9fbcef57a26
Sobo_uo43 <Sobo_uo43@student.itstep.org>
2025-01-25 12:10:50 +0200

    Added appuser service

---
+++ src/main/java/com/example/cinemate/service/busines/appuserservice/AppUserService.java
@@ -0,0 +1,13 @@
+package com.example.cinemate.service.busines.appuserservice;
+
+import com.example.cinemate.model.AppUser;
+import java.util.List;
+
+public interface AppUserService {
+    void save(AppUser user);
+    int[] saveUsersList(List<AppUser> users);
+    void update(AppUser user);
+    void delete(AppUser user);
+    List<AppUser> findAll();
+    void deleteAll();
+}
---
+++ src/main/java/com/example/cinemate/service/busines/appuserservice/AppUserServiceImpl.java
@@ -0,0 +1,45 @@
+package com.example.cinemate.service.busines.appuserservice;
+
+import com.example.cinemate.dao.appuser.AppUserRepository;
+import com.example.cinemate.model.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class AppUserServiceImpl implements AppUserService {
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Override
+    public void save(AppUser user) {
+        appUserRepository.save(user);
+    }
+
+    @Override
+    public int[] saveUsersList(List<AppUser> users) {
+        appUserRepository.saveAll(users);
+        return new int[0];
+    }
+
+    @Override
+    public void update(AppUser user) {
+        appUserRepository.save(user);
+    }
+
+    @Override
+    public void delete(AppUser user) {
+        appUserRepository.delete(user);
+    }
+
+    @Override
+    public List<AppUser> findAll() {
+        return appUserRepository.findAll();
+    }
+
+    @Override
+    public void deleteAll() {
+        appUserRepository.deleteAll();
+    }
+}
